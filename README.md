6184638e49a625fff10066c0ab9fc054cdc74229
Chai Phonbopit <phonbopit@gmail.com>
2015-02-04 16:32:38 +0700

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+## Hello Hapi
+
+ตัวอย่างการใช้งาน Hapi.js จากบทความ [ทดลองใช้ Hapi.js สร้าง RESTFul API แบบง่ายๆ](http://devahoy.com/2015/02/create-restful-api-with-hapi-js/)
---
+++ app.js
@@ -0,0 +1,53 @@
+var Hapi = require('hapi');
+var users = require('./users');
+
+var server = new Hapi.Server();
+server.connection({
+  host: 'localhost',
+  port: 7777
+});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function(request, reply) {
+    reply('<h1>Hello Hapi :)</h1');
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/users',
+  handler: function(request, reply) {
+    reply(users.findAll());
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/user/{id}',
+  handler: function(request, reply) {
+    var id = request.params.id;
+    reply(users.findById(id));
+  }
+});
+
+server.route({
+  method: 'POST',
+  path: '/newuser',
+  handler: function(request, reply) {
+    console.log('hello ' + request.payload.username);
+    user = {
+      id: request.payload.id,
+      username: request.payload.username,
+      name: request.payload.name,
+      position: request.payload.position
+    }
+    users.save(user);
+    reply(user);
+  }
+});
+
+server.start(function() {
+  console.log('Hapi running at: ', server.info.uri);
+});
